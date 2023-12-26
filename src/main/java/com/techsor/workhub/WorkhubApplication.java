b51b70ac15b7b2f794055590235af4260dedaa7c
jiangAB <jiangjin514@foxmail.com>
2023-12-26 09:21:54 +0800

    ertert

--- src/main/java/com/techsor/workhub/WorkhubApplication.java
+++ src/main/java/com/techsor/workhub/WorkhubApplication.java
@@ -1,11 +1,9 @@
 package com.techsor.workhub;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.techsor.workhub.mapper")
 public class WorkhubApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/techsor/workhub/controller/StaffController.java
+++ src/main/java/com/techsor/workhub/controller/StaffController.java
@@ -1,14 +1,14 @@
 package com.techsor.workhub.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StaffController {
-
         @GetMapping(value = "/staff")
         public String getStaffInfo() {
-
             return "数据库查询";
         }
 }
